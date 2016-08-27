f4e6035f9d4aa3074b78a82615d2161419880514
Joel Lopez <jowie.jowie@gmail.com>
2016-08-28 01:14:41 +0200

    Add initial game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MandarineStudio.AncientTreaseures
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance
+        {
+            get
+            {
+                if (m_instance == null)
+                    m_instance = Instantiate(new GameManager());
+                return m_instance;
+            }
+        }
+
+        public EventSystem EventSystem
+        {
+            get { return m_eventSystem; }
+        }
+
+        public float Score
+        {
+            get { return m_score; }
+        }
+
+        private static GameManager m_instance;
+        private EventSystem m_eventSystem = new EventSystem();
+
+        [SerializeField] private float m_score = 0f;
+
+        public void TakeGem()
+        {
+            m_score += 1f;
+        }
+
+        public void SaveGame()
+        {
+            
+        }
+    }
+}
