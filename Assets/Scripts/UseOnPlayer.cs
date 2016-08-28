c0301920790b660eff2d1844080c55948ab9fe28
Joel Lopez <jowie.jowie@gmail.com>
2016-08-28 20:49:23 +0200

    Script to launch actions when player presses Use button

---
+++ Assets/Scripts/UseOnPlayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MandarineStudio.AncientTreasures
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class UseOnPlayer : MonoBehaviour
+    {
+        public string FunctionToCall = "";
+        public bool OneShot = true;
+
+        private bool m_playerIn;
+
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.tag == "Player")
+                m_playerIn = true;
+        }
+
+        void OnTriggerExit2D(Collider2D col)
+        {
+            if (col.tag == "Player")
+                m_playerIn = false;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            m_playerIn = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (m_playerIn && Input.GetButtonDown("Use"))
+            {
+                BroadcastMessage(FunctionToCall);
+                if (OneShot)
+                    enabled = false;
+            }
+        }
+    }
+}
