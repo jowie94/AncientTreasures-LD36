06262134ae82cacaeac3fcb99700df5700a261e8
Joel Lopez <jowie.jowie@gmail.com>
2016-08-29 01:15:46 +0200

    Add mummy and add scorpion prefab

---
+++ Assets/Scripts/MummyControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace MandarineStudio.AncientTreasures
+{
+    public class MummyControl : EnemyControl
+    {
+        private Rigidbody2D m_rigidbody;
+
+        void Start()
+        {
+            m_rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        protected override void FeetDamage(Damage damage) {}
+
+        protected override void WeaponDamage(Damage damage)
+        {
+            Controller.Animator.onStop.AddListener(Restore);
+            Controller.Animator.Play("Die", true);
+            m_rigidbody.isKinematic = true;
+            enabled = false;
+            Controller.Life = 0;
+        }
+
+        void Restore()
+        {
+            Controller.Animator.onStop.RemoveListener(Restore);
+            Controller.Life = 1;
+            enabled = true;
+            m_rigidbody.isKinematic = false;
+        }
+    }
+}
