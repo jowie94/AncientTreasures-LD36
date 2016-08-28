f0854c6b94a981e512bc0ad669c84e1b06366a2b
Joel Lopez <jowie.jowie@gmail.com>
2016-08-28 17:52:23 +0200

    Death trigger prefab

---
+++ Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MandarineStudio.AncientTreasures
+{
+    public class DeathTrigger : MonoBehaviour
+    {
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                other.GetComponent<PlatformerCharacterController>().Die(true);
+            }
+        }
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
