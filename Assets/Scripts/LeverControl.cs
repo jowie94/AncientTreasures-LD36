dbad41c2838a6ea22abe9a9954a078518d781a12
Joel Lopez <jowie.jowie@gmail.com>
2016-08-29 15:11:44 +0200

    Add lever prefab

---
+++ Assets/Scripts/LeverControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace MandarineStudio.AncientTreasures
+{
+    [RequireComponent(typeof(UseOnPlayerTrigger), typeof(SpriteAnimator))]
+    public class LeverControl : MonoBehaviour
+    {
+        public string EventName;
+
+        private SpriteAnimator m_animator;
+
+        void Awake ()
+        {
+            m_animator = GetComponent<SpriteAnimator>();
+        }
+
+        void ActivateLever()
+        {
+            m_animator.Play("Move", true);
+            m_animator.onStop.AddListener(Action);
+        }
+
+        void Action()
+        {
+            GameManager.Instance.EventSystem.Trigger(EventName);
+            m_animator.onStop.RemoveListener(Action);
+            m_animator.Play("On", true);
+        }
+    }
+}
