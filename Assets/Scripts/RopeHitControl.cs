4a1d9a2e16ef988e5841852796d4fe677ebc1ffe
Joel Lopez <jowie.jowie@gmail.com>
2016-08-29 17:32:01 +0200

    Add new assets and improve rope cut

---
+++ Assets/Scripts/RopeHitControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MandarineStudio.AncientTreasures
+{
+    [RequireComponent(typeof(HitTrigger))]
+    public class RopeHitControl : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start ()
+        {
+            GameManager.Instance.EventSystem.Subscribe(GetComponent<HitTrigger>().EventName, () => gameObject.SetActive(false));
+        }
+    }
+}
