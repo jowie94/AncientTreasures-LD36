d7e63e189a8aa45d605551c4d7e2f6512e6575b8
Joel Lopez <jowie.jowie@gmail.com>
2016-08-29 23:01:30 +0200

    Destroy on event script

---
+++ Assets/Scripts/DestroyOnEvent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace MandarineStudio.AncientTreasures
+{
+    public class DestroyOnEvent : MonoBehaviour
+    {
+        public string EventName;
+
+        // Use this for initialization
+        void Start () {
+            if (string.IsNullOrEmpty(EventName))
+            {
+                Debug.LogError("Trigger name is not specified, disabling trigger " + name);
+                enabled = false;
+            }
+            GameManager.Instance.EventSystem.Subscribe(EventName, () => Destroy(gameObject));
+        }
+    }
+}
