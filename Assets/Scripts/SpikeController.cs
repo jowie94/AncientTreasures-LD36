8a85c478020eca8986031559404c5c7208d9bad4
Joel Lopez <jowie.jowie@gmail.com>
2016-08-29 13:00:20 +0200

    Add spikes

---
+++ Assets/Scripts/SpikeController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace MandarineStudio.AncientTreasures
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class SpikeController : MonoBehaviour
+    {
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.tag == "Player")
+                GameManager.Instance.KillPlayer();
+        }
+        // Use this for initialization
+        void Awake() {
+    
+        }
+    
+        // Update is called once per frame
+        void Update () {
+    
+        }
+    }
+}
